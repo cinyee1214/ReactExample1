b55ad29387d6aa472125c25d3c0866ba927f5796
cinyee1214 <xinyizhao1214@gmail.com>
2020-07-16 19:23:38 -0400

    JavaScript Way

--- src/App.js
+++ src/App.js
@@ -49,32 +49,38 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+    
+    if (this.state.showPersons) {
+      persons = (
+        <div >
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}>My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div >
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is really working!</p>
         <button 
           style={style}
-          onClick={this.togglePersonHandler}>Toggle Persons</button>
-        { 
-        this.state.showPersons ? 
-          <div >
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Max!')}
-              changed={this.nameChangedHandler}>My Hobbies: Racing
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            />
-          </div > : null
-        }
+          onClick={this.togglePersonHandler}>Toggle Persons</button >
+
+          {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
