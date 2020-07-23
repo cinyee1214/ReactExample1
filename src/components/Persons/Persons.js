6a41712a464abf874460a7c30f87d807c04cc9b0
cinyee1214 <xinyizhao1214@gmail.com>
2020-07-23 18:03:13 -0400

    PureComponent

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return (
+class Persons extends PureComponent {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
         <Person
-          click={() => props.clicked(index)}
-          name={person.name}
-          age={person.age}
-          key={person.id}
-          changed={event => props.changed(event, person.id)}
-        />
-    );
+            click={() => this.props.clicked(index)}
+            name={person.name}
+            age={person.age}
+            key={person.id}
+            changed={event => this.props.changed(event, person.id)}
+          />  
+      );
+    }
+  );
   }
-);
 
-export default persons;
+}
+
+export default Persons;
